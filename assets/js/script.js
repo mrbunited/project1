613271e3f6990f0ce791cddeb25bd2cd2237acc3
d-car <danielscarstensen@gmail.com>
2018-07-16 23:11:00 -0500

    added geolocation, working on making search functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -114,12 +114,43 @@ function initMap() {
   //map
   var map = new google.maps.Map(document.getElementById("map"), options);
 
-  //add marker
-  var marker = new google.maps.Marker({
-    position:{lat:21.8789, lng:-87.6359},
-    map:map
-  });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
+
+  var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+  };
+
 
 
 
